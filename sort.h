90a83f436d6350dad3af88c416a84495bf03f35a
Kevin-M254 <kmburu007@gmail.com>
2023-01-24 22:38:12 +0300

    function that sorts an array of integers in ascending order using the Quick sort algorithm

--- sort.h
+++ sort.h
@@ -38,5 +38,6 @@ void insertion_sort_list(listint_t **list);
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */

1bcaf0420048c8b95ef9075ecc24e81af22c0470
Kevin-M254 <kmburu007@gmail.com>
2023-01-24 17:08:05 +0300

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- sort.h
+++ sort.h
@@ -33,6 +33,7 @@ typedef struct listint_s
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);

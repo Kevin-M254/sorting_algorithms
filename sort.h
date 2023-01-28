f63464d1f503ac5a784738898fa514cd6f34788a
Kevin-M254 <kmburu007@gmail.com>
2023-01-28 15:33:37 +0300

    counting sort algorithm

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * enum bool - Enumeration of Boolean values.
@@ -41,5 +42,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */

9c9f37c0911bca0f97ce3de195464544ffee7624
Kevin-M254 <kmburu007@gmail.com>
2023-01-28 14:53:54 +0300

    update header

--- sort.h
+++ sort.h
@@ -40,5 +40,6 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
